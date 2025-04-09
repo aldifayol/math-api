0476acbadc7ab5aa277434b923d8abb53355d862
Aldi <aldifayol@gmail.com>
2025-04-09 20:51:03 +0800

    unit testing using jest

---
+++ src/MathBasic.js
@@ -0,0 +1,11 @@
+const MathBasic = {
+  add: function add() {},
+
+  subtract: function subtract() {},
+
+  multiply: function multiply() {},
+
+  divide: function divide() {},
+};
+
+module.exports = MathBasic;
